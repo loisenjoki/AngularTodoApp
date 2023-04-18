85d8e474440c20909ec03d6b49ad1a032553b962
Loice Mwai <Loice@Loice-MacBook-Pro.local>
2023-04-18 16:26:49 +0300

    created the UI components and project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './modules/task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Angular Todo app';
+
+  filter: 'all' | 'uncomplete' | 'completed' = 'all';
+
+  allTasks = [
+    { description: 'eat', completed: true },
+    { description: 'sleep', completed: false },
+    { description: 'play', completed: false },
+    { description: 'laugh', completed: false },
+  ];
+
+
+  get tasks() {
+    if (this.filter === 'all') {
+      return this.allTasks;
+    }
+    return this.allTasks.filter((task) => this.filter === 'completed' ? task.completed : !task.completed);
+  }
+
+  addTask(description: string) {
+    this.allTasks.unshift({
+      description,
+      completed: false
+    });
+  }
+
+  remove(item: Task) {
+    this.allTasks.splice(this.allTasks.indexOf(item), 1);
+  }
 }
